ce80b60d3c30de06ab4e4ff498f4a22dd9e636cb
bwlee13 <brandonlee2013@gmail.com>
2023-12-13 16:22:15 -0500

    day2-part2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from .days.day1 import *
+from .days.day2 import *
 
 def aoc_solver():
-    print("Aoc solving...")
-    output = day1_part1()
+    print("\nAoc solving...\n")
+    output = day2_part2()
+    print("Output...") 
+    print("_________\n") 
+    print("Result:\n") 
     print(output) 
-
+    print("_________\n") 
+    print("End...\n") 
 
 
 if __name__ == '__main__':
